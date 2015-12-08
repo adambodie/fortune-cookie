9c783a9ede2db5efa48c04270ca1f643e7575fa3
Adam Bodie <aduo3000@yahoo.com>
2015-12-07 20:04:28 -0800

    Added numberController.js

--- js/controllers/fortuneController.js
+++ js/controllers/fortuneController.js
@@ -8,19 +8,7 @@ app.controller('fortuneController', ['$scope', function($scope) {
     $scope.number = function () {
 		return $scope.age - ($scope.firstName + $scope.lastName).length   
     }
-    
-    $scope.lucky = function () {
-		$scope.random = Math.floor(Math.random() * 10 + 6);
-		$scope.parseAge = Number.parseInt($scope.age);    	
-		$scope.one = $scope.lastName.length * $scope.random;
-		$scope.two = $scope.firstName.length * $scope.random;
-		$scope.three = $scope.parseAge + $scope.random; 
-		$scope.four = Math.floor($scope.one + $scope.parseAge + ($scope.random));
-		$scope.five = Math.floor($scope.two + $scope.parseAge + ($scope.random));
-		$scope.six = Math.abs($scope.one + $scope.two - $scope.three + $scope.four - $scope.five);
-		return $scope.one + " " + $scope.two + " " + $scope.three + " " + $scope.four + " " + $scope.five + " " + $scope.six;   
-    
-    }
+
     $scope.toggle = function() {
         if ($scope.firstName == "" || $scope.lastName == "" || $scope.age == "") {
             alert("Please enter a value")
