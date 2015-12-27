31c3745bb72fb5915ebdb84058f7d38f9330915b
Adam Bodie <aduo3000@yahoo.com>
2015-12-27 14:41:15 -0800

    Updated fortuneController.js

--- js/controllers/fortuneController.js
+++ js/controllers/fortuneController.js
@@ -1,28 +1,30 @@
 app.controller('fortuneController', ['$scope', function($scope) {
-    $scope.firstName = "John",
-    $scope.lastName = "Doe"
-    $scope.age = 30;
-    $scope.mySwitch = false;
-    $scope.myVar = false;
+	$scope.firstName = "John",
+	$scope.lastName = "Doe"
+	$scope.age = 30;
+	$scope.mySwitch = false;
+	$scope.myVar = false;
     
-    $scope.number = function () {
+	$scope.number = function () {
 		return $scope.age - ($scope.firstName + $scope.lastName).length   
-    }
-
-    $scope.toggle = function() {
-        if ($scope.firstName == "" || $scope.lastName == "" || $scope.age == "") {
-            alert("Please enter a value")
-        } else {
-            $scope.myVar = !$scope.myVar;
-            $scope.mySwitch = true;
-        }
-    }
-    $scope.reset = function() {
-        $scope.firstName = "",
-        $scope.lastName = ""
-        $scope.age = "";
-        $scope.myVar = false;
-        $scope.mySwitch = false;
+	}
+	$scope.random = function(){
+	
+	}
+	$scope.toggle = function() {
+		if ($scope.firstName == "" || $scope.lastName == "" || $scope.age == "") {
+      	alert("Please enter a value")
+      } else {
+      	$scope.myVar = !$scope.myVar;
+         $scope.mySwitch = true;
+      }
+	}
+   $scope.reset = function() {
+   	$scope.firstName = "",
+       $scope.lastName = ""
+       $scope.age = "";
+       $scope.myVar = false;
+       $scope.mySwitch = false;
     }
 }]);
 
