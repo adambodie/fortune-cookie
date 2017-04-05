d00b6c325c8d73031b01f18eb95c1dbb23249697
Adam Bodie <aduo3000@yahoo.com>
2017-04-04 18:08:11 -0700

    Added Sass and CssMin Grunt Tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-        all: ['Gruntfile.js', 'js/**/*.js']
+        all: ['Gruntfile.js', 'js/**/app.js']
+      },
+    sass: {
+      dist: {
+        files: {
+          './dist/css/main.css': './src/css/main.scss'
+        }
       }
-      });
+    },
+    cssmin: {
+        options: {
+          mergeIntoShorthands: false,
+          roundingPrecision: -1
+        },
+        target: {
+          files: {
+            './dist/css/main.min.css': './dist/css/main.css'
+          }
+        }
+}
+  });
      grunt.loadNpmTasks('grunt-contrib-jshint');
+     grunt.loadNpmTasks('grunt-contrib-sass');
+     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-     grunt.registerTask('default', ['jshint']);
+     grunt.registerTask('default', ['jshint', 'sass', 'cssmin']);
 
 };
