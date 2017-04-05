31d65b9dc03bc5fb33c66f304c1b8231933ebc4f
adambodie <aduo3000@yahoo.com>
2017-04-04 20:24:41 -0700

    Added Watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,28 @@ module.exports = function(grunt) {
             './dist/css/main.min.css': './dist/css/main.css'
           }
         }
-}
+	},
+	watch: {
+		options: {
+		dateFormat: function(time) {
+			  grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+			  grunt.log.writeln('Waiting for more changes...');
+			},
+		  },		
+		scripts: {
+			files: ['**/*.js'],
+			tasks: ['jshint'],
+			options: {
+				spawn: false,
+			},
+		},
+	}
   });
      grunt.loadNpmTasks('grunt-contrib-jshint');
      grunt.loadNpmTasks('grunt-contrib-sass');
      grunt.loadNpmTasks('grunt-contrib-cssmin');
+     grunt.loadNpmTasks('grunt-contrib-watch');
 
-     grunt.registerTask('default', ['jshint', 'sass', 'cssmin']);
+     grunt.registerTask('default', ['jshint', 'sass', 'cssmin', 'watch']);
 
 };
