e850c6ee8c3914b6efb9a72b83d30122b7620e77
Adam Bodie <aduo3000@yahoo.com>
2017-04-09 14:17:27 -0700

    Updated FortuneController with new functions, combined with NumberController

--- dist/js/controllers/fortuneController.js
+++ dist/js/controllers/fortuneController.js
@@ -1,30 +1,77 @@
-app.controller('fortuneController', ['$scope', function($scope) {
-	$scope.firstName = "",
-	$scope.lastName = ""
+app.controller('FortuneController', ['$scope', '$http', function($scope, $http) {
+	$scope.firstName = "";
+	$scope.lastName = "";
 	$scope.age = "";
 	$scope.mySwitch = false;
 	$scope.myVar = false;
-    
+
+	$scope.random = Math.round(Math.random() * 50);
+	$scope.randomTwo = Math.round(Math.random() * 10);
+
+	$scope.getFortunes = function (index){
+	        $http.get('./src/data/fortunes.json')
+	            .then(function (result) {
+									$scope.fortuneCookie = result.data.fortune[index].value;
+									return $scope.fortuneCookie;
+	            });
+	    };
+
 	$scope.number = function () {
-		return $scope.age - ($scope.firstName + $scope.lastName).length   
+		$scope.fortuneValue = Number.parseInt($scope.age) - ($scope.firstName + $scope.lastName).length;
+		if ($scope.fortuneValue + $scope.random > 100) {
+			$scope.fortune = (100 - $scope.fortuneValue + $scope.random);
+		} else {
+			$scope.fortune = $scope.fortuneValue + $scope.random;
+		}
+		return $scope.fortune;
 	}
-	$scope.random = function(){
-	
+
+	$scope.luckyOne = function() {
+		return $scope.lastName.length * $scope.randomTwo;
 	}
-	$scope.toggle = function() {
+
+	$scope.luckyTwo = function() {
+		return Math.floor(1000 / ($scope.firstName.length * $scope.randomTwo));
+	}
+
+	$scope.luckyThree = function() {
+		$scope.parseAge = Number.parseInt($scope.age);
+		return $scope.parseAge + $scope.randomTwo;
+	}
+
+	$scope.luckyFour = function () {
+		$scope.parseAge = Number.parseInt($scope.age);
+		return $scope.randomTwo + ($scope.parseAge + $scope.lastName.length);
+	}
+
+	$scope.luckyFive = function () {
+		$scope.parseAge = Number.parseInt($scope.age);
+		return $scope.randomTwo + ($scope.parseAge + $scope.firstName.length);
+	}
+
+	$scope.luckySix = function () {
+		$scope.parseAge = Number.parseInt($scope.age);
+		return Math.floor(($scope.parseAge * ($scope.lastName.length + $scope.firstName.length)/$scope.randomTwo));
+	}
+
+	$scope.submit = function() {
 		if ($scope.firstName == "" || $scope.lastName == "" || $scope.age == "") {
       	alert("Please enter a value")
       } else {
       	$scope.myVar = !$scope.myVar;
-         $scope.mySwitch = true;
+        $scope.mySwitch = true;
+				$scope.answer = $scope.getFortunes($scope.number());
+				$scope.lucky = [$scope.luckyOne(), $scope.luckyTwo(), $scope.luckyThree(), $scope.luckyFour(), $scope.luckyFive(), $scope.luckySix()];
+				$scope.luckyNumbers = $scope.lucky.join(' ');
       }
 	}
    $scope.reset = function() {
-   	$scope.firstName = "",
-       $scope.lastName = ""
+   		 $scope.firstName = "";
+       $scope.lastName = "";
        $scope.age = "";
        $scope.myVar = false;
        $scope.mySwitch = false;
+			 $scope.random = Math.round(Math.random() * 100)
+			 $scope.randomTwo = Math.round(Math.random() * 10);
     }
 }]);
-
--- dist/js/services/fortune.js
+++
@@ -1,9 +0,0 @@
-app.factory('fortune', ['$http', function($http) {
-  return $http.get('./data/fortune.json')
-            .success(function(data) {
-              return data;
-            })
-            .error(function(err) {
-              return err;
-            });
-}]);
